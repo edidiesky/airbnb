0aac8a6da2be34d24b50eb8d6d2dd6fccc48bc92
edidiesky <essienedidiong1000@gmail.com>
2023-08-03 22:22:19 -0700

    new fixes

--- frontend/src/components/dashboard/guests/index.jsx
+++ frontend/src/components/dashboard/guests/index.jsx
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 import React from "react";
+import HostGuestsLeftIndex from "./left";
+import HostGuestsRightIndex from "./right";
 
 export default function HostGuestsIndex() {
   return (
     <>
       <HostGuestsIndexPlaceContainer className="flex item-center justify-center">
+        <div className="w-90 auto grid_wrapper">
+          <HostGuestsLeftIndex />
+          <HostGuestsRightIndex />
+        </div>
       </HostGuestsIndexPlaceContainer>
     </>
   );
@@ -12,4 +18,13 @@ export default function HostGuestsIndex() {
 
 const HostGuestsIndexPlaceContainer = styled.div`
   width: 100%;
+  .grid_wrapper {
+    display: grid;
+    grid-template-columns: 29vw 1fr;
+    grid-gap: 2rem;
+    place-items: start;
+    @media (max-width: 780px) {
+      grid-template-columns: 1fr;
+    }
+  }
 `;
